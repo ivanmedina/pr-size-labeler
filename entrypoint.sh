4ddb69f06985cad37046ff03431eda57954defad
Ivan Medina <ivansk8.imm@gmail.com>
2022-06-09 19:17:21 -0500

    fix: more bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,7 +57,7 @@ comment() {
         -H "${API_HEADER}" \
         -X POST \
         -H "Content-Type: application/json" \
-        -d "{\"\body\":\"${comment}\"}" \
+        -d '{"body":"Probando"}' \
         "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}/comments"
 }
 
